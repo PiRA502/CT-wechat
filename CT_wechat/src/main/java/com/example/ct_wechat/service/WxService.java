334c2426a49595deb6d590248bad94a752b092a2
PiRA502 <43596587+PiRA502@users.noreply.github.com>
2022-07-15 16:07:36 +0800

    1.1
    消息全部转成map格式
    修改菜单（测试中）

--- CT_wechat/src/main/java/com/example/ct_wechat/service/WxService.java
+++ CT_wechat/src/main/java/com/example/ct_wechat/service/WxService.java
@@ -1,5 +1,9 @@
 package com.example.ct_wechat.service;
 
+import com.example.ct_wechat.msg.BaseMessage;
+import com.example.ct_wechat.msg.ImageMsg;
+import com.example.ct_wechat.msg.TextMsg;
+import com.thoughtworks.xstream.XStream;
 import org.dom4j.Document;
 import org.dom4j.DocumentException;
 import org.dom4j.Element;
@@ -35,4 +39,109 @@ public class WxService {
         }
         return map;
     }
+
+    /*
+     *封装公众号返回信息，用于处理所有的事件和消息的回复，返回的是xml数据包
+     * */
+    public static String getResponse(Map<String, String> requestMap) {
+        BaseMessage msg = null;
+        //获取用户发送消息的类型
+        String msgType = (String) requestMap.get("MsgType");
+        switch (msgType) {
+            case "text":
+                //处理文本类型
+                msg = dealTextMessage(requestMap);
+                //将图文信息转成xml数据包
+                //msg=dealNewsMessage(requestMap);
+                //System.out.println("---返回文字类型数据---");
+                break;
+            case "image":
+                //处理图片信息
+                msg = dealImageMessage(requestMap);
+                break;
+            case "event":
+                //处理各类事件
+                msg = dealEvent(requestMap);
+                break;
+            default:
+                break;
+        }
+        if (msg != null) {
+            //将信息转成xml数据包
+            return beanToXml(msg);
+        }
+        return null;
+    }
+
+    /*
+     * 把消息对象转成xml数据
+     * */
+    private static String beanToXml(BaseMessage msg) {
+        XStream stream = new XStream();
+        /*
+         * 使对应类上的注解生效
+         * */
+        stream.processAnnotations(BaseMessage.class);
+        stream.processAnnotations(TextMsg.class);
+        stream.processAnnotations(ImageMsg.class);
+        String xml = stream.toXML(msg);
+        return xml;
+    }
+
+    /*
+     * 专门处理文本消息
+     * */
+    private static BaseMessage dealTextMessage(Map<String, String> requestMap) {
+        TextMsg textMessage=new TextMsg(requestMap,"Got your text.");
+        return textMessage;
+    }
+
+    /*
+     *
+     *专门处理回复图片消息 requestMap是从用户从公众号发过来的数据
+     * */
+    private static BaseMessage dealImageMessage(Map<String, String> requestMap) {
+        TextMsg textMessage=new TextMsg(requestMap,"Got the picture.");
+        return textMessage;
+    }
+
+    private static BaseMessage dealEvent(Map<String, String> requestMap) {
+        //判断什么事件
+        String event = requestMap.get("Event");
+        switch (event) {
+            case "CLICK":
+                //详细处理点击事件
+                return dealClick(requestMap);
+            default:
+                //其它事件
+                break;
+        }
+        return null;
+    }
+
+    /**
+     * 专门处理点击事件
+     * @param requestMap
+     * @return
+     */
+    private static BaseMessage dealClick(Map<String, String> requestMap) {
+        //获取创建菜单时填的key值
+        String key = requestMap.get("EventKey");
+        switch (key){
+            case "1":
+                //点击一级菜单
+                return new TextMsg(requestMap,"点击了一级菜单");
+            case "21":
+                //点击二级菜单的1
+                return new TextMsg(requestMap,"点击二级菜单的1");
+            case "22":
+                //点击二级菜单的2
+                return new TextMsg(requestMap,"点击二级菜单的2");
+            default:
+                break;
+        }
+        return null;
+    }
+
+
 }
